0d8cda0a8e335a99b4af51654abc4e086fbb4d08
Niles-Rondez <142612332+Niles-Rondez@users.noreply.github.com>
2025-05-28 21:06:10 +0800

    added different modes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const button = document.getElementById("start");
+
 let timerInterval;
 let timeLeft = 25 * 60; // 25 minutes in seconds
 
@@ -35,3 +36,36 @@ function pauseTimer() {
     clearInterval(timerInterval);
     button.textContent = "START";
 }
+
+document.querySelector(".modes").addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON") {
+        document.querySelectorAll(".modes button").forEach(btn => btn.classList.remove("active"));
+        event.target.classList.add("active");
+
+        clearInterval(timerInterval);
+        timeLeft = 0; // Reset time left
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+        if (button.textContent === "PAUSE") {
+            button.textContent = "START";
+        }
+        button.textContent = "START";
+        const mode = event.target.dataset.mode;
+        switch (mode) {
+            case "pomodoro":
+                document.documentElement.style.setProperty('--primary-color', 'rgb(199, 73, 73)');
+                timeLeft = 25 * 60; // 25 minutes
+                break;
+            case "short-break":
+                document.documentElement.style.setProperty('--primary-color', 'rgb(70, 145, 141)');
+                timeLeft = 5 * 60; // 5 minutes
+                break;
+            case "long-break":
+                document.documentElement.style.setProperty('--primary-color', 'rgb(75, 113, 145)');
+                timeLeft = 15 * 60; // 15 minutes
+                break;
+        }
+        minutes.textContent = String(Math.floor(timeLeft / 60)).padStart(2, '0');
+        seconds.textContent = String(timeLeft % 60).padStart(2, '0');
+    }
+});
